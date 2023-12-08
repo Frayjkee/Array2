fd5a256bb5ed23870b0b0e2824ccd761fe0c2702
Ilya Maksimov <iluxamax@yandex.ru>
2023-12-08 13:36:32 +0300

    finish

--- Задача2.cs
+++ Задача2.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем генератор случайных чисел
+        Random random = new Random();
+
+        // Задаем размер массива
+        int arraySize = 10;
+
+        // Создаем массив и заполняем его случайными числами
+        int[] numbers = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            numbers[i] = random.Next(1, 101); // Генерируем случайное число от 1 до 100
+        }
+
+        // Выводим массив на экран
+        Console.WriteLine("Сгенерированный массив:");
+        foreach (var number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        // Определяем количество четных чисел в массиве
+        int evenCount = CountEvenNumbers(numbers);
+
+        // Выводим результат
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+
+        // Ждем, пока пользователь не нажмет Enter, чтобы консольное окно не закрылось
+        Console.ReadLine();
+    }
+
+    // Метод для определения количества четных чисел в массиве
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
